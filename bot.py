445f3777ec82717fe6db2aa632ac20eab4f1e4b9
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-01-10 19:08:32 -0200

    Removed keyboard to better support sharing

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import logging
 import config
 from digrss import Digrss
 from telegram import Bot
-from telegram import InlineKeyboardButton as Button
-from telegram import InlineKeyboardMarkup as Keyboard
 
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(name)s - %(message)s')
@@ -13,9 +11,9 @@ bot = Bot(config.TOKEN)
 with Digrss(interval=config.INTERVAL) as g:
     for entry in g:
 
-        msg = "<b>{}</b>\n\n{}".format(entry.title, entry.summary)
+        msg = f"<b>{entry.title}</b>\n\n{entry.summary}"
+        msg += f"\n[{config.MORE}]({entry.link})"
         msg = msg.replace('<br>', '\n')
 
-        keyboard = Keyboard([[Button(config.MORE, url=entry.link)]])
-        bot.send_message(entry.target, text=msg, reply_markup=keyboard,
-                         disable_web_page_preview=False, parse_mode="HTML")
+        bot.send_message(entry.target, text=msg, parse_mode="HTML",
+                         disable_web_page_preview=False)
