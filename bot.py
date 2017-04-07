23c05844ab876a4f9338171a871ce721fbcdc7ed
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-04-07 00:20:11 -0300

    Minor changes

--- bot.py
+++ bot.py
@@ -4,16 +4,17 @@ from digrss import Digrss
 from telegram import Bot
 
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(name)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] %(name)s - %(message)s')
 
 bot = Bot(config.TOKEN)
 
 with Digrss(interval=config.INTERVAL) as g:
     for entry in g:
 
-        msg = f"<b>{entry.title}</b>\n\n{entry.summary}"
-        msg += f"\n[{config.MORE}]({entry.link})"
+        msg = "<b>{}</b>\n\n{}".format(entry.title, entry.summary)
+        msg += "\n<a href=\"{}\">{}</a>".format(entry.link, config.MORE)
         msg = msg.replace('<br>', '\n')
 
         bot.send_message(entry.target, text=msg, parse_mode="HTML",
-                         disable_web_page_preview=False)
+                         disable_web_page_preview=True)
